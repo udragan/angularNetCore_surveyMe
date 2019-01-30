3cde54ffb16a3b4b4b34fcbfe2299ccfcd9b2ffc
udragan <ujfa2001@yahoo.com>
2019-01-30 19:23:37 +0100

    added default IdentityServer4.DeviceController and its dependencies

---
+++ solution/SurveyMe/SurveyMe.Auth/Models/Device/DeviceAuthorizationInputModel.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+// Namespace changed
+
+using udragan.netCore.SurveyMe.Auth.Models.Consent;
+
+namespace udragan.netCore.SurveyMe.Auth.Models.Device
+{
+	public class DeviceAuthorizationInputModel : ConsentInputModel
+	{
+		public string UserCode { get; set; }
+	}
+}
---
+++ solution/SurveyMe/SurveyMe.Auth/Models/Device/DeviceAuthorizationViewModel.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+// Namespace changed
+
+using udragan.netCore.SurveyMe.Auth.Models.Consent;
+
+namespace udragan.netCore.SurveyMe.Auth.Models.Device
+{
+	public class DeviceAuthorizationViewModel : ConsentViewModel
+	{
+		public string UserCode { get; set; }
+		public bool ConfirmUserCode { get; set; }
+	}
+}
