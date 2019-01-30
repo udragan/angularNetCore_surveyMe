bc6ca22b76b84e20c181a364c1cf4712d41ad2fd
udragan <ujfa2001@yahoo.com>
2019-01-30 19:34:44 +0100

    added default IdentityServer4.DiagnosticsController and its dependencies

---
+++ solution/SurveyMe/SurveyMe.Auth/Controllers/DiagnosticsController.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using udragan.netCore.SurveyMe.Auth.Attributes;
+using udragan.netCore.SurveyMe.Auth.Models.Diagnostics;
+
+namespace udragan.netCore.SurveyMe.Auth.Controllers
+{
+	[SecurityHeaders]
+	[Authorize]
+	public class DiagnosticsController : Controller
+	{
+		public async Task<IActionResult> Index()
+		{
+			var localAddresses = new string[] { "127.0.0.1", "::1", HttpContext.Connection.LocalIpAddress.ToString() };
+			if (!localAddresses.Contains(HttpContext.Connection.RemoteIpAddress.ToString()))
+			{
+				return NotFound();
+			}
+
+			var model = new DiagnosticsViewModel(await HttpContext.AuthenticateAsync());
+			return View(model);
+		}
+	}
+}
