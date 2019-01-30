eeb4819a7f6b2410572fc7aca68eb4453de3b410
udragan <ujfa2001@yahoo.com>
2019-01-30 18:47:36 +0100

    added default IdentityServer4.ConsentController and its dependencies

---
+++ solution/SurveyMe/SurveyMe.Auth/Models/Consent/ConsentInputModel.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+// Namespace changed
+
+using System.Collections.Generic;
+
+namespace udragan.netCore.SurveyMe.Auth.Models.Consent
+{
+	public class ConsentInputModel
+	{
+		public string Button { get; set; }
+		public IEnumerable<string> ScopesConsented { get; set; }
+		public bool RememberConsent { get; set; }
+		public string ReturnUrl { get; set; }
+	}
+}
