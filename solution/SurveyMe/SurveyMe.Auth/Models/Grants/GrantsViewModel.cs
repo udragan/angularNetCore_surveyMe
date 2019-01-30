812bb96db5a9dd5f40c9cb5d877d5f8d4f25d5e9
udragan <ujfa2001@yahoo.com>
2019-01-30 19:48:36 +0100

    dded default IdentityServer4.HomeController and its dependencies

--- solution/SurveyMe/SurveyMe.Auth/Models/Grants/GrantsViewModel.cs
+++ solution/SurveyMe/SurveyMe.Auth/Models/Grants/GrantsViewModel.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+// Namespace changed
+
+using System;
 using System.Collections.Generic;
 
 namespace udragan.netCore.SurveyMe.Auth.Models.Grants
