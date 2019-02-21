4b783c3f86641dd5b99992f084f86397b95a8900
udragan <ujfa2001@yahoo.com>
2019-02-21 19:20:19 +0100

    added dummy SurveyController to api

---
+++ solution/SurveyMe/SurveyMe.API/Controllers/SurveyController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SurveyMe.API.Controllers
+{
+	[Route("api/[controller]")]
+	[Produces("application/json")]
+	public class SurveyController : ControllerBase
+	{
+		public SurveyController()
+		{ }
+
+		[Authorize]
+		[HttpGet("all")]
+		public async Task<IActionResult> GetAll()
+		{
+			IList<string> result = new List<string>
+			{
+				"test"
+			};
+
+			return Ok(result);
+		}
+	}
+}
--- solution/SurveyMe/SurveyMe.API/Startup.cs
+++ solution/SurveyMe/SurveyMe.API/Startup.cs
@@ -1,40 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace udragan.netCore.SurveyMe.API
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 
-        public IConfiguration Configuration { get; }
+		public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
-        }
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddMvcCore()
+				.AddAuthorization()
+				.AddJsonFormatters();
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+			services.AddAuthentication("Bearer")
+				.AddJwtBearer("Bearer", options =>
+				{
+					options.Authority = "http://localhost:2884";
+					options.RequireHttpsMetadata = false;
 
-            app.UseMvc();
-        }
-    }
+					options.Audience = "SurveyMe.API";
+				});
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.UseMvc();
+		}
+	}
 }
